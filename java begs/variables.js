809ea314eaa0112d7ecb990babe3edcfb559d31d
Haya <haya.ehab.dev@gmail.com>
2025-05-28 15:43:24 +0300

    Add files via upload

---
+++ java begs/variables.js
@@ -0,0 +1,12 @@
+const dog = {
+    name: 'Max',
+    breed: 'German',
+    age: 5,
+    weightInPounds: 12,
+    eat: function() {
+     console.log('Chomp!');
+    },
+    bark() {
+        console.log('Woof!');
+    }
+}
