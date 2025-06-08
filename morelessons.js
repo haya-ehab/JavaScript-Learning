594b04b16a53376ded61745ebb70560f7489e4b3
Haya <haya.ehab.dev@gmail.com>
2025-06-08 20:42:50 +0300

    added new lessons and modified the html file

---
+++ morelessons.js
@@ -0,0 +1,21 @@
+// if customer has more than 100 points,
+// they are gold customer otherwise,
+// they are silver customer
+
+// let points = 90;
+// let type = points  > 100 ? 'gold' : 'silver';
+
+
+// console.log(type);
+
+
+
+let a = 'red';
+let b = 'blue';
+
+let c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b);
