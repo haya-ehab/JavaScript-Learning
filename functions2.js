5f88ec7ebc9b081c4c01b3b3bc34f0d4b4d0ae52
Haya <haya.ehab.dev@gmail.com>
2025-06-27 17:32:12 +0300

    added Lessons to Functions2.js

---
+++ functions2.js
@@ -0,0 +1,36 @@
+//  Function Declarations vs Expressions
+
+// Function Declartion
+
+function walk() {
+
+    console.log('walk');
+
+}
+
+// Function Expression
+// let or const
+let run = function() {
+    console.log('run');
+};
+
+
+// Hoisting
+// What is Hoisting? 
+// Hoisting is moving the function to the top of the file
+
+// Arguments
+
+// function sum() {
+
+//     let total = 0;
+//     for (let value of arguments)
+//         total += value;
+//     return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 10));
+
+
+// The Rest Operator
+
